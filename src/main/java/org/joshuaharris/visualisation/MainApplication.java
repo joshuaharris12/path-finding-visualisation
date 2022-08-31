a01f8807fdfd3d6b831c774d52385a794624e2c6
Joshua Harris <joshuakharris12@gmail.com>
2022-08-31 20:24:37 +0100

    Update entry point in Main Application

--- src/main/java/org/joshuaharris/visualisation/MainApplication.java
+++ src/main/java/org/joshuaharris/visualisation/MainApplication.java
@@ -1,11 +1,21 @@
 package org.joshuaharris.visualisation;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.joshuaharris.visualisation.UI.View;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        View view = new View(10,10);
 
+        BorderPane root = new BorderPane(view.getView());
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Visualising Path Finding Algorithms");
+        primaryStage.show();
     }
 }
